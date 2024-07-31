ebbed9f91e965ef99a52801dee063597592d375d
kileo123 <kileo123@yahoo.com>
2024-07-31 12:53:33 +0700

    Updates
    Update Readme and links to respective pages
    Added Web02
    Update Web01 css

--- README.md
+++ README.md
@@ -3,12 +3,14 @@ Hello Odin!
 
 ## 2024
 
+### July 31st
+Created [Web02](/Web02) for Rock-Paper-Scissor game, no css
 ### July 30th
 testing multiple credentials
 - [x] windows
 - [x] linux
 ### July 26th
-Created Web01 web
+Created [Web01](/Web01) static responsive web
 ### July 25th
 Created OdinP Repo
 
---
+++ Web02/rps.js
@@ -0,0 +1,80 @@
+let computerChoice = getComputerChoice();
+let whoWin;
+let message;
+let humanChoice = 1;
+let computerScore = 0;
+let humanScore = 0;
+let winCondition = 't'
+
+function play(bVal){
+  var v = bVal.value;
+  if (v === "Rock") {
+    humanChoice = 1
+  } else if ( v === "Paper") {
+    humanChoice = 2
+  } else {
+    humanChoice = 3
+  }
+  computerChoice = getComputerChoice()
+  whoWin = checkWinCondition()
+  message = printChoice(humanChoice)+" vs "+printChoice(computerChoice)+" -- "+whoWin
+  message = message +" -- User "+humanScore+" : Computer "+computerScore
+  // console.log(message)
+  var ul = document.createElement("ul")
+  var result = document.createTextNode(message)
+  ul.appendChild(result)
+  document.getElementById("result").appendChild(ul)
+}
+
+function getComputerChoice(){
+  return(((Math.floor(Math.random()*1000*Math.random()))%3)+1);
+}
+
+// 1 = Rock, 2 = Paper , 3 = Scissor
+function printChoice(c){
+  if (c === 1) { return "Rock" }
+  else if (c === 2) { return "Paper" }
+  else { return "Scissor" }
+}
+
+function checkWinCondition(){
+  if (computerChoice === 1){
+    if (humanChoice === 2){
+      winCondition = 'h'
+    } else if (humanChoice === 3) {
+      winCondition = 'c'
+    } else {
+      winCondition = 't'
+    }
+  } else if (computerChoice === 2){
+    if (humanChoice === 3){
+      winCondition = 'h'
+    } else if (humanChoice === 1) {
+      winCondition = 'c'
+    } else {
+      winCondition = 't'
+    }
+  } else if (computerChoice === 3){
+    if (humanChoice === 1){
+      winCondition = 'h'
+    } else if (humanChoice === 2) {
+      winCondition = 'c'
+    } else {
+      winCondition = 't'
+    }
+  }
+  if (winCondition === 'h') {
+    humanScore++
+    return "User Win"
+  } else if (winCondition === 'c') {
+    computerScore++
+    return "Computer Win"
+  } else {
+    winCondition = 't'
+    return "TIE"
+  }
+}
+
+
+
+
