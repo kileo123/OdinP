8a8856c763fb7f5d1b4d5eb45edbcf03d4f979b8
kileo123 <kileo123@yahoo.com>
2024-09-19 20:32:17 +0700

    Odin Tic-Tac-Toe

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Hello Odin!
 ## 2024
 
 ### Sep 18th 
+- Updated Odin Tic-Tac-Toe
+  - Currently basic gameplay only, will add:
+    - Player name input
+    - Reset once game end
+    - Score keeping
+### Sep 18th 
 - Added [Odin Tic-Tac-Toe](https://kileo123.github.io/OdinP/Web03/ttt.html)
   - Not working yet, only empty board as of now
 ### Sep 17th 
--- Web03/ttt.js
+++ Web03/ttt.js
@@ -1,15 +1,69 @@
 const tttBoard = (() => {
   console.log("inside tttboard")
-  const board = document.getElementById("board")
   const header = document.getElementById("header")
-  const boardCells = new Array(9).fill("")
-  
   header.textContent = "Odin Tic-Tac-Toe"
 
-  boardCells.forEach((cell, index) => {
+  const board = document.getElementById("board")
+  const boardCells = new Array(9).fill("")
+  
+  boardCells.forEach((_, index) => {
     const cellElement = document.createElement("div")
     cellElement.classList.add("cells");
-    cellElement.id = `cell${index+1}`
+    cellElement.id = `cell-${index+1}`
+    // cellElement.addEventListener("click", addGo)
     board.append(cellElement)
   })
+
+  const footer = document.getElementById("footer")
+  footer.innerHTML = `circle goes first`
+
+  startGame();
 })()
+
+function startGame() {
+  console.log("lets start")
+  const allCells = document.querySelectorAll(".cells")
+  const cells = Array.from(allCells)
+  cells.forEach((_cell, index) => {
+    cells[index].addEventListener("click", playTurn)
+  })
+}
+
+var turn = "circle"
+
+function playTurn(e) {
+  const play = document.createElement("div")
+  play.classList.add(turn)
+  e.target.append(play)
+  turn = turn === "circle" ? "cross" : "circle"
+  e.target.removeEventListener("click", playTurn)
+
+  const footer = document.getElementById("footer")
+  footer.textContent = `${turn} turn now`
+  checkWin();
+}
+
+function checkWin() {
+  const allCells = document.querySelectorAll(".cells")
+  const winningCombos = [[0,1,2], [3,4,5], [4,5,6],[0,3,6], [1,4,7], [2,5,8],[0,4,8], [2,4,6]]
+
+  winningCombos.forEach(array => {
+    const circleWins = array.every(cell => allCells[cell].firstChild?.classList.contains("circle"))
+    const crossWins = array.every(cell => allCells[cell].firstChild?.classList.contains("cross"))
+    if (circleWins){
+      footer.textContent = "CIRCLE WINS !!!"
+      allCells.forEach(cell => cell.replaceWith(cell.cloneNode(true)))
+    } else if (crossWins){
+      footer.textContent = "CROSS WINS !!!"
+      allCells.forEach(cell => cell.replaceWith(cell.cloneNode(true)))
+    } 
+  })
+
+  var empty = 9
+  allCells.forEach((_, index) => {
+    if(allCells[index].firstChild != null) {
+      empty--
+    }
+  })
+  empty === 0 ? footer.textContent = "IT'S A TIE !!!" : console.log("game continue")
+}
