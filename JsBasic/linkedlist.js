0e61b705225240b189e02d5e95c7e25acb0df9cb
kileo123 <kileo123@yahoo.com>
2024-10-10 18:34:35 +0700

    testing

---
+++ JsBasic/linkedlist.js
@@ -0,0 +1,195 @@
+class Node {
+  constructor(value = null) {
+      this.value = value;
+      this.nextNode = null;
+  }
+}
+
+export class LinkedList {
+  constructor() {
+    this.size = 0;
+    this.tail = null;
+    this.head = null;
+  }
+
+  getHead() {
+    return this.tail;
+  }
+  getTail() {
+    return this.head;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      const temp = this.head;
+      this.head = newNode;
+      this.head.nextNode = temp;
+    }
+    if (this.tail == null) {
+      this.tail = newNode;
+    }
+
+    this.size += 1;
+  }
+
+  prepend(value) {
+    let newNode = new Node(value);
+    if (this.tail == null) {
+      this.tail = newNode;
+      if (this.head == null) {
+        this.head = newNode;
+      }
+    } else {
+      const temp = this.tail;
+      temp.nextNode = newNode;
+      this.tail = newNode;
+    }
+    this.size += 1;
+  }
+
+  at(index) {
+    function nodeAtIndex(index, count, node) {
+      if (index === count) {
+        return node;
+      } else if (node.nextNode == null) {
+        return null;
+      } else {
+        return nodeAtIndex(index, count + 1, node.nextNode);
+      }
+    }
+    return nodeAtIndex(index, 0, this.head);
+  }
+
+  pop() {
+    if (this.head.nextNode == null) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.nextNode;
+    }
+    this.size -= 1;
+  }
+
+  contains(value) {
+    function searchNodes(node, value) {
+      if (node.value === value) {
+        return true;
+      } else if (node.nextNode == null) {
+        return false;
+      } else {
+        return searchNodes(node.nextNode, value);
+      }
+    }
+    return searchNodes(this.head, value);
+  }
+
+  find(value) {
+    let index = 0;
+    function searchNodes(node, value, index) {
+      if (node.value === value) {
+        return index;
+      } else if (node.nextNode == null) {
+        return null;
+      } else {
+        return searchNodes(node.nextNode, value, index + 1);
+      }
+    }
+    return searchNodes(this.head, value, index);
+  }
+
+  insertAt(value, index) {
+    let previousNode = null;
+    let newNode = new Node(value);
+    let currentNode = this.head;
+    let count = 0;
+
+    if (index === 0) {
+      this.append(value);
+      return;
+    }
+
+    while (currentNode != null) {
+      if (count == index) {
+        if (currentNode.nextNode == null) {
+          this.prepend(value);
+          return;
+        } else {
+          let nextNode = currentNode;
+          previousNode.nextNode = newNode;
+          newNode.nextNode = nextNode;
+          return;
+        }
+      }
+
+      previousNode = currentNode;
+      currentNode = currentNode.nextNode;
+      count += 1;
+    }
+  }
+
+  removeAt(index) {
+    let previousNode = null;
+    let currentNode = this.head;
+    let count = 0;
+
+    if (index === 0) {
+      this.pop();
+      return;
+    }
+
+    while (currentNode != null) {
+      if (count == index) {
+        if (currentNode.nextNode == null) {
+          previousNode.nextNode = null;
+          return;
+        } else {
+          previousNode.nextNode = currentNode.nextNode;
+          return;
+        }
+      }
+
+      previousNode = currentNode;
+      currentNode = currentNode.nextNode;
+      count += 1;
+    }
+  }
+
+  toString() {
+    let nodes = [];
+    let current = this.head;
+
+    while (current != null) {
+      nodes.push(current.value);
+      current = current.nextNode;
+    }
+
+    console.log(nodes.join(' -> ') + ' -> null');
+  }
+
+  remove(value) {
+    let count = 0;
+    let previousNode = null;
+    let currentNode = this.head;
+
+    if (this.head.value === value) {
+      this.pop();
+      return;
+    }
+
+    while (currentNode) {
+      if (currentNode.value === value) {
+        previousNode.nextNode = currentNode.nextNode;
+        return;
+      }
+      previousNode = currentNode;
+      currentNode = currentNode.nextNode;
+    }
+  }
+}
